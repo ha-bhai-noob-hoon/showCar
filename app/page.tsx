5b6e74e48d60ddd539ab957cd0b2c2c1a612a09a
Abhijit Pandey <abhijitpandey524@gmail.com>
2023-06-18 18:54:56 +0530

    random message

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 
 
 
@@ -10,7 +10,22 @@ export default function Home() {
 
   <div className='mt-12 padding-x padding-y max-width' id='discover'>
     <div className='home__text-container'>
+      <h1 className='text-4xl font-extrabold'>
+        Car Catalogue
+      </h1>
+      <p>
+        Explore the cars you might like
+      </p>
+    </div>
 
+    <div className='home__filters'>
+      <SearchBar />
+
+    </div>
+
+    <div className='home__filter-container'>
+      <CustomFilter title="fuel" />
+      <CustomFilter title="year" />
     </div>
   </div>
 
---
+++ components/SearchBar.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useState } from 'react';
+import { SearchManufacturer } from './';
+
+
+const SearchBar = () => {
+    const [manufactutrer , setManufacturer] = useState('');
+
+
+    const handleSearch = () => {
+
+    }
+
+    return ( 
+        <form className="searchbar" onSubmit={handleSearch} action="">
+            <div className="searchbar_item">
+                <SearchManufacturer 
+                    manufacturer = {manufactutrer}
+                    setManufacturer = {setManufacturer}
+                />
+            </div>
+        </form>
+     );
+}
+ 
+export default SearchBar;
